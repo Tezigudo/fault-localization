304f513efff0bfb06d49f6b982cb5b649c930e63
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2024-07-24 17:50:20 +0900

    add dataset to realworld project and start to analyzing

---
+++ convert.py
@@ -0,0 +1,14 @@
+import json
+
+# Read the input JSON file
+with open('line_of_code.json', 'r') as input_file:
+    data = json.load(input_file)
+
+# Create a new dictionary with the desired format
+output_data = {file['path']: file['stats']['total'] for file in data['files']}
+
+# Write the new dictionary to an output JSON file
+with open('line_of_code1.json', 'w') as output_file:
+    json.dump(output_data, output_file, indent=4)
+
+print("Conversion complete. Check the output.json file.")
