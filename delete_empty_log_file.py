226eeb96daa54db3bb379c670c7d78c13cd80235
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2024-07-30 20:12:10 +0900

    addded overall graph

---
+++ delete_empty_log_file.py
@@ -0,0 +1,13 @@
+import os
+
+def delete_empty_logs(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.log'):
+                file_path = os.path.join(root, file)
+                if os.path.getsize(file_path) == 0:
+                    print(f"Deleting empty log file: {file_path}")
+                    os.remove(file_path)
+
+directory = "percentage_of_removed_log"
+delete_empty_logs(directory)
